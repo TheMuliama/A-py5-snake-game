3b7de944bdbf864bf1e7975c82e8c69b6af3346d
TheMuliama <themuliama@proton.me>
2024-05-17 19:02:26 +0400

    adding the head movement
    Making it that the head can move and that it shows properly on the game window.

--- snake.py
+++ snake.py
@@ -1,18 +1,20 @@
 import py5
 
-WORLD_GRID_HEIGHT = 10
-WORLD_GRID_WIDTH = 10
+WORLD_GRID_HEIGHT = 20
+WORLD_GRID_WIDTH = 20
 CELL_SIZE = 10
 
-class player():
+class player_class():
     def __init__(self):
-        self.head_position = [0,0]
+        self.head_position_x = 4
+        self.head_position_y = 5
         self.speed_x = 1
         self.speed_y = 0
+        self.score = 0
     
     def update_position(self):
-        self.head_position[0] = self.head_position[0] + self.speed_x
-        self.head_position[1] = self.head_position[1] + self.speed_y
+        self.head_position_x = self.head_position_x + self.speed_x
+        self.head_position_y = self.head_position_y + self.speed_y
 
     def set_speed(self,new_speed_x, new_speed_y):
         self.speed_x = new_speed_x
@@ -28,14 +30,22 @@ def draw_grid():
                 py5.square(i * CELL_SIZE, j * CELL_SIZE, CELL_SIZE)
 
 def setup():
-    global world_grid
+    global world_grid, player
     py5.size(WORLD_GRID_WIDTH * CELL_SIZE, WORLD_GRID_HEIGHT * CELL_SIZE)
     py5.background(0)
-    world_grid = clear_world_grid()
     py5.no_stroke()
+    player = player_class()
+    world_grid = clear_world_grid()
 
 def draw():
-    clear_world_grid()
+    global player, world_grid, tmp
+
+    py5.background(0)
+    world_grid = clear_world_grid()
+    player.set_speed(player.speed_x * -1, 0)
+    player.update_position()
+    world_grid[player.head_position_x][player.head_position_y] = 1
+    
     draw_grid()
 
 py5.run_sketch()
