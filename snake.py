23acbbf7bca7f0b9170c1b2307c5917569cbc23a
TheMuliama <themuliama@proton.me>
2024-05-19 13:02:08 +0400

    adding the body parts
    I added the body part into the game, and I made sure that if I add a body part it will add itself properly.
    Before changing the direction of the player, the program now check if it doesn't do a turn-around.
    I also added a function to check if the player bumped into itself.

--- snake.py
+++ snake.py
@@ -7,19 +7,29 @@ GAME_COOLDOWN = 20
 
 class player_class():
     def __init__(self):
-        self.head_position_x = 4
-        self.head_position_y = 5
+        self.head_position_x = WORLD_GRID_WIDTH//2
+        self.head_position_y = WORLD_GRID_HEIGHT//2
+        self.body = [[self.head_position_x-1, self.head_position_y], [self.head_position_x-2, self.head_position_y], [self.head_position_x-3, self.head_position_y]]
         self.speed_x = 1
         self.speed_y = 0
         self.score = 0
     
     def update_position(self):
+        for i in range(len(self.body)-1, 0, -1):
+            self.body[i] = self.body[i-1]
+        self.body[0] = [self.head_position_x, self.head_position_y]
         self.head_position_x = self.head_position_x + self.speed_x
         self.head_position_y = self.head_position_y + self.speed_y
 
     def set_speed(self,new_speed_x, new_speed_y):
         self.speed_x = new_speed_x
         self.speed_y = new_speed_y
+    
+    def is_self_colliding(self):
+        for body_part in self.body:
+            if body_part == [self.head_position_x, self.head_position_y]:
+                return True
+        return False
 
 def clear_world_grid():
     return [[0 for j in range(WORLD_GRID_WIDTH)] for i in range(WORLD_GRID_HEIGHT)]
@@ -34,25 +44,33 @@ def update_world():
     global player, world_grid, current_cooldown
     world_grid = clear_world_grid()
     player.update_position()
+    
     if border_collision() == True:
         print("The player touched the border!!! \n")
+
+    if player.is_self_colliding():
+        print("The player bumped into himself!!! \n")
+
+    for body_part in player.body:
+        world_grid[body_part[1]][body_part[0]] = 1
+    
     world_grid[player.head_position_y][player.head_position_x] = 1
     current_cooldown = GAME_COOLDOWN
 
 def key_pressed():
     global player, world_grid
 
-    if py5.key_code == py5.UP:
+    if py5.key_code == py5.UP and player.speed_y != 1:
         player.set_speed(0,-1)
 
-    if py5.key_code == py5.DOWN:
+    if py5.key_code == py5.DOWN and player.speed_y != -1:
         player.set_speed(0,1)
 
-    if py5.key_code == py5.RIGHT:
-        player.set_speed(1,0)
+    if py5.key_code == py5.LEFT and player.speed_x != 1:
+        player.set_speed(-1,0)    
 
-    if py5.key_code == py5.LEFT:
-        player.set_speed(-1,0)
+    if py5.key_code == py5.RIGHT and player.speed_x != -1:
+        player.set_speed(1,0)
 
 def border_collision():
     global player
